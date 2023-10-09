4ed5f3cc7293f323d6130a1acd1bf8ea0e7a4851
devnoob547 <danielalvespessoa3006@gmail.com>
2023-10-09 17:34:20 -0300

    initial setup

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const prettierConfig = {
+  singleQuote: true,
+  endOfLine: 'crlf',
+  arrowParens: 'avoid',
+  semi: true,
+};
+
+module.exports = prettierConfig;
